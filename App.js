2dc33a58821f556a2411f55a43552176ae8e5166
Teo <teowenlong0316@gmail.com>
2021-10-16 16:39:45 +0800

    finalise master

--- App.js
+++ App.js
@@ -1,15 +1,7 @@
 import React, { useState } from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  TextInput,
-  Text,
-  View,
-  Button,
-  TouchableOpacity,
-  Alert
-} from 'react-native';
+import { SafeAreaView, StyleSheet, TextInput, Text, View, TouchableOpacity, Alert } from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
+import { useForm, Controller } from 'react-hook-form';
 
 const listData = [
   { label: 'Male', value: 'male' },
@@ -17,60 +9,99 @@ const listData = [
 ]
 
 const App = () => {
-
   const [listOpen, setListOpen] = useState(false);
-  const [formValue, setFormValue] = useState({
-    name: '',
-    email: '',
-    gender: '',
-  });
+  const { control, handleSubmit, formState: { errors } } = useForm();
 
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
-        <View style={styles.textBox}>
-          <TextInput
-            style={styles.text}
-            placeholder="Name"
-            defaultValue={formValue.name}
-            onChangeText={(item) => setFormValue({
-              ...formValue,
-              name: item
-            })}
-          />
-        </View>
-        {/* <Text style={styles.errorText}>Error</Text> */}
-
-        <View style={styles.textBox}>
-          <TextInput
-            style={styles.text}
-            placeholder="Email"
-            defaultValue={formValue.email}
-            onChangeText={(item) => setFormValue({
-              ...formValue,
-              email: item
-            })}
-          />
-        </View>
-        {/* <Text style={styles.errorText}>Error</Text> */}
-
-        <DropDownPicker
-          style={styles.dropdown}
-          placeholder="Select your gender"
-          open={listOpen}
-          setOpen={itemValue => setListOpen(itemValue)}
-          items={listData}
-          value={formValue.gender}
-          setValue={(item) => setFormValue({
-            ...formValue,
-            gender: item(),
-          })}
+        <Controller
+          control={control}
+          name='name'
+          render={({ field: { onChange, value } }) => (
+            <View style={styles.textBox}>
+              <TextInput
+                style={styles.text}
+                placeholder="Name"
+                defaultValue={value}
+                onChangeText={(v) => onChange(v)}
+              />
+            </View>
+          )}
+          rules={{
+            required: {
+              value: true,
+              message: 'Please fill out all required fields.'
+            },
+          }}
         />
-        {/* <Text style={styles.errorText}>Error</Text> */}
+
+        {errors['name']?.message ?
+          <Text style={styles.errorText}>{errors['name']?.message}</Text>
+          : null}
+
+        <Controller
+          control={control}
+          name='email'
+          render={({ field: { onChange, value } }) => (
+            <View style={styles.textBox}>
+              <TextInput
+                style={styles.text}
+                placeholder="Email"
+                defaultValue={value}
+                onChangeText={(v) => onChange(v)}
+              />
+            </View>
+          )}
+          rules={{
+            required: {
+              value: true,
+              message: 'Please fill out all required fields.'
+            },
+            pattern: {
+              value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+              message: 'Please enter valid email.'
+            },
+          }}
+        />
+
+        {errors['email']?.message ?
+          <Text style={styles.errorText}>{errors['email']?.message}</Text>
+          : null}
+
+        <Controller
+          control={control}
+          name='gender'
+          render={({ field: { onChange, value } }) => (
+            <DropDownPicker
+              style={styles.dropdown}
+              placeholder="Select your gender"
+              placeholderStyle={styles.dropdownPlaceholder}
+              open={listOpen}
+              setOpen={() => setListOpen(!listOpen)}
+              items={listData}
+              value={value}
+              setValue={(item) => onChange(item())}
+            />
+          )}
+          rules={{
+            required: {
+              value: true,
+              message: 'Please fill out all required fields.'
+            }
+          }}
+        />
+
+        {errors['gender']?.message ?
+          <Text style={styles.errorText}>{errors['gender']?.message}</Text>
+          : null}
+
       </View>
 
       <View style={styles.container}>
-        <TouchableOpacity style={styles.button} onPress={() => Alert.alert("Form Value", JSON.stringify(formValue))}>
+        <TouchableOpacity style={styles.button} onPress={
+          handleSubmit((formValue) => Alert.alert("Form Value", JSON.stringify(formValue)))
+        }>
           <Text style={styles.buttonText}>Submit</Text>
         </TouchableOpacity>
       </View>
@@ -108,6 +139,9 @@ const styles = StyleSheet.create({
     borderColor: '#e4e4e4',
     backgroundColor: '#fafafa',
     marginVertical: 10
+  },
+  dropdownPlaceholder: {
+    color: '#c7c7c8',
   },
   button: {
     width: '100%',
